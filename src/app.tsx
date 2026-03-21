89eb5b768335b30cd6d01151f2eeae9b344a5eaf
IP <ilya.pavlovski@gmail.com>
2026-03-21 15:52:51 -0300

    First.

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import styles from './app.module.css'
+
+export default function App() {
+
+  return (
+    <section className={styles.container}>
+      <h1>Hello World</h1>
+    </section>
+  )
+}
